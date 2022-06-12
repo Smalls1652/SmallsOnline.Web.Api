e801cc2c0d48adb2d5bfbb9f0800aa420eb3f756
Timothy Small <smalls.online@icloud.com>
2022-06-12 18:20:29 -0400

    Made separate async methods

--- src/SmallsOnline.Web.Api/services/favorites-of/tracks/CosmosDbService_GetFavoriteTracksOfYear.cs
+++ src/SmallsOnline.Web.Api/services/favorites-of/tracks/CosmosDbService_GetFavoriteTracksOfYear.cs
@@ -7,31 +7,31 @@ public partial class CosmosDbService : ICosmosDbService
 {
     public List<TrackData> GetFavoriteTracksOfYear(string listYear)
     {
-        Task<List<TrackData>> getFromDbTask = Task.Run(
-            async () =>
-            {
-                List<TrackData> trackItems = new();
-
-                Container container = cosmosDbClient.GetContainer(AppSettings.GetSetting("CosmosDbContainerName"), "favorites-of");
-                QueryDefinition query = new($"SELECT * FROM c WHERE c.partitionKey = \"favorites-of-tracks\" AND c.listYear = \"{listYear}\"");
-
-                FeedIterator<TrackData> containerQueryIterator = container.GetItemQueryIterator<TrackData>(query);
-                while (containerQueryIterator.HasMoreResults)
-                {
-                    foreach (TrackData item in await containerQueryIterator.ReadNextAsync())
-                    {
-                        trackItems.Add(item);
-                    }
-                }
-
-                containerQueryIterator.Dispose();
-
-                return trackItems;
-            }
-        );
+        Task<List<TrackData>> getFromDbTask = Task.Run(async () => await GetFavoriteTracksOfYearAsync(listYear));
 
         getFromDbTask.Wait();
 
         return getFromDbTask.Result;
+    }
+
+    public async Task<List<TrackData>> GetFavoriteTracksOfYearAsync(string listYear)
+    {
+        List<TrackData> trackItems = new();
+
+        Container container = cosmosDbClient.GetContainer(AppSettings.GetSetting("CosmosDbContainerName"), "favorites-of");
+        QueryDefinition query = new($"SELECT * FROM c WHERE c.partitionKey = \"favorites-of-tracks\" AND c.listYear = \"{listYear}\"");
+
+        FeedIterator<TrackData> containerQueryIterator = container.GetItemQueryIterator<TrackData>(query);
+        while (containerQueryIterator.HasMoreResults)
+        {
+            foreach (TrackData item in await containerQueryIterator.ReadNextAsync())
+            {
+                trackItems.Add(item);
+            }
+        }
+
+        containerQueryIterator.Dispose();
+
+        return trackItems;
     }
 }
--- src/SmallsOnline.Web.Api/services/interfaces/ICosmosDbService.cs
+++ src/SmallsOnline.Web.Api/services/interfaces/ICosmosDbService.cs
@@ -6,5 +6,8 @@ namespace SmallsOnline.Web.Api.Services;
 public interface ICosmosDbService
 {
     List<AlbumData> GetFavoriteAlbumsOfYear(string listYear);
+    Task<List<AlbumData>> GetFavoriteAlbumsOfYearAsync(string listYear);
+    
     List<TrackData> GetFavoriteTracksOfYear(string listYear);
+    Task<List<TrackData>> GetFavoriteTracksOfYearAsync(string listYear);
 }
